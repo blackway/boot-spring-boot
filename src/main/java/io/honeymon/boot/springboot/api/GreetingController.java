7c1cadeacb6766c5fa7ddd0e6aa373f454837e14
임대림 <blackway76@gmail.com>
2019-11-15 20:43:12 +0900

    test

--- src/main/java/io/honeymon/boot/springboot/api/GreetingController.java
+++ src/main/java/io/honeymon/boot/springboot/api/GreetingController.java
@@ -8,6 +8,6 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting() {
-        return "Hello, Boot Spring Boot!!";
-    }
+        return "Hello, Boot Spring Boot!! 임대림 1";
+    } 
 }
